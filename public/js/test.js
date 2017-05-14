ac61487f72cdc2bcdb65f23b3f0d652397f0c58a
Stefano Fumanti <fumanti@hotmail.com>
2017-05-14 20:14:01 +0200

    Archivia

--- public/js/test.js
+++ public/js/test.js
@@ -18,7 +18,7 @@ function getList(url, sort){
   }
   
   $.ajax({
-    url: url === 'test?page=2' ? 'test?page=1' : url,
+    url: 'test?page=2',
     type: "post",
       data: {
         'owner': ($('#owner span[data-bind="label"]').text()=='Tutti i test')?'all':'user',
